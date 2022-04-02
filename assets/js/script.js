f0737a01ac6326b7e8482a00d09630ac1038ec3f
NicolasLoontjens <nicolas.loontjens@student.howest.be>
2022-04-02 15:59:58 +0200

    added login form, now handling the submitting of the data

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,56 @@
 document.addEventListener("DOMContentLoaded",init);
 
 function init(){
-    
+    isLoggedIn();
+}
+
+async function isLoggedIn(){
+    if(!await localforage.getItem('loggedin')){
+        showStart();
+    }else{
+        showHome();
+    }
+}
+
+function showStart(){
+    //hide the buttons in nav, display register or login button;
+    document.querySelectorAll("header div *:nth-child(2)").forEach(elem => {
+        elem.classList.add("hidden");
+    });
+    clearMain();
+    //display template tag
+    const template = document.querySelector("#template-start-buttons");
+    document.querySelector("main").appendChild(template.content.cloneNode(true)); 
+    //add event listeners
+    document.querySelector("#registeruser").addEventListener("click",addStartForm);
+    document.querySelector("#loginuser").addEventListener("click",addStartForm);
+}
+
+function addStartForm(e){
+    e.preventDefault();
+    clearMain();
+    console.log(e.target.getAttribute("value"));
+    const template = document.querySelector("#template-start-form");
+    document.querySelector("main").appendChild(template.content.cloneNode(true));
+    if(e.target.getAttribute('value') === 'register'){
+        document.querySelector("start-form-submit").addEventListener("click",registerUser);
+    }else{
+        document.querySelector("start-form-submit").addEventListener("click",loginUser);
+    }
+}
+
+async function registerUser(e){
+    e.preventDefault();
+}
+
+async function loginUser(e){
+    e.preventDefault();
+}
+
+function clearMain(){
+    document.querySelector("main").innerHTML = "";
+}
+
+function showHome(){
+    //here we show all the posts, etc
 }
